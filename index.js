9d9239e2e22c73d483178137034251df9fad8d6e
ibird <yuanshaopeng01@gmail.com>
2013-06-20 22:04:41 +0800

    重写了index.js

--- index.js
+++ index.js
@@ -1,176 +1,128 @@
-	//将json字符串转化为json对象
-	var json = eval ("(" + json + ")");
+var typeArr = ['pc', 'kindle', 'phone', 'laptop', 'others']
+var flowArr = ['1','2','3','4','5']
 
-	//将data.json中的数据输出为html	
-	showJson = function(obj) {
-
-		var total = []
-		total['flow'] = 0
-		total['down'] = 0
-		total['up'] = 0
-
-		var html = "<table><tr><td>姓名</td><td>总流量(M)</td><td>已用流量(M)</td><td>设备名</td><td>设备Mac地址</td><td>上传量(M)</td><td>下载量(M)</td><td colspan='4'>操作</td></tr>"
-		document.write(html)
-
-		for (x in obj.club)
-		{
-			var device_n = obj.club[x].device.length + 1
-			var totalFlow = parseInt(obj.club[x].total / 1024 / 1024 )
-			var nowFlow = parseInt(obj.club[x].nowflow / 1024 / 1024)
-			total['flow'] += nowFlow
-
-			var html = "<tr><td rowspan='"+device_n+"'>"+obj.club[x].name+"</td><td rowspan='"+device_n+"'>"+totalFlow+"</td><td id='" + x + "_flow' rowspan='"+device_n+"'>"+nowFlow+"</td><td></td><td></td><td></td><td></td><td colspan='2'><input type='button' value='新增设备' name='"+ obj.club[x].id+"'  onclick='add(this)'/></td><td rowspan='"+device_n+"'><input type='button' value='修改流量' name='"+ obj.club[x].id+"'  onclick='changeFlow(this)'/></td><td rowspan='"+device_n+"'><input type='button' value='删除用户' name='"+ obj.club[x].id+"'  onclick='delUser(this)'/></td></tr>"
-
-
-			document.write(html)
-
-			for ( y in obj.club[x].device)
-			{
-				i = obj.club[x].device[y]
-				down = (parseInt)(i['out'] / 1024 / 1024)
-				up = (parseInt)(i['in'] / 1024 /1024)
-				total['down'] += down
-				total['up'] += up
-
-				var html = "<td>"+i.type+"</td><td>"+i.mac+"</td><td>"+up+"</td><td>"+down+"</td><td><input type='button' value='修改Mac' name='"+obj.club[x].id+"' onclick='change(this)'/></td><td><input type='button' value='删除设备' name='"+obj.club[x].id+"' onclick='delDev(this)'/></td></tr>"
-				document.write(html)
-
-			}
-
-			var nowID = parseInt(obj.club[x].id) + 1
-		}
-
-		html = "<tr><td></td><td></td><td colspan='5'></td><td colspan='4'><input type='button' value='新增用户' name='" + nowID + "' onclick='addUser(this)' /></td></tr>"
-		document.write(html);
-
-		html = "<tr><td >总计</td><td>30720</td><td>"+total['flow']+"</td><td colspan='2'></td><td>"+total['up']+"</td><td>"+total['down']+"</td><td colspan='4'><input type='button' value='清零流量' onclick='cleanFlow()' /></td></tr></table>"
-		document.write(html)
-
-		document.write("<div id='time' style='display:none'></div>")
-	}
-	
-	//ajax方法
-	var ajax = function(url, f, string){ 
-		var xml = (window.XMLHttpRequest) ? (new XMLHttpRequest()) : (new ActiveXObject("Micorsoft.XMLHttpRequest"));
-        	xml.onreadystatechange = function(){
-          		f(xml.responseText);
-        	}
-
-    		xml.open("POST",url,true);
-    		xml.setRequestHeader("Content-type","application/x-www-form-urlencoded");	
-		xml.send(string);
-		}
-
-	//增加设备
-	var add = function(obj){
-		a = obj.parentNode.parentNode
-		var arr = ['pc', 'kindle', 'phone', 'laptop', 'others']
-		var select = initSelect(arr)
-		a.childNodes[3].appendChild(select)
-
-		var input = document.createElement("input")
-		input.setAttribute('type', 'text')	
-		a.childNodes[4].appendChild(input)
-
-		obj.setAttribute('value','确定')
-		obj.setAttribute('onclick','addSubmit(this)')
-	}
-	
-	//初始化select标签，参数为option的value值的数组
-	var initSelect = function( arr )
+//初始化select标签，参数为option的value值的数组
+var initSelect = function( arr )
+{
+	var select = document.createElement('select')
+	for(i in arr)
 	{
-		var select = document.createElement('select')
-		for(i in arr)
-		{
-			var option = document.createElement('option')
-			option.setAttribute('value', arr[i])
-			option.text = arr[i]
-			select.appendChild(option)
-		}
-		return select
-	}	
+		var option = document.createElement('option')
+		option.setAttribute('value', arr[i])
+		option.text = arr[i]
+		select.appendChild(option)
+	}
+	return select
+}	
+
+//ajax方法
+var ajax = function(url, f, string){ 
+	var xml = (window.XMLHttpRequest) ? (new XMLHttpRequest()) : (new ActiveXObject("Micorsoft.XMLHttpRequest"));
+	xml.onreadystatechange = function(){
+  		f(xml.responseText);
+	}
+
+	xml.open("POST",url,true);
+	xml.setRequestHeader("Content-type","application/x-www-form-urlencoded");	
+	xml.send(string);
+}
+
+//替换节点
+var changeId = function(id, newtype)
+{
+	old = document.getElementById(id)
+	if(old.childNodes)
+	{
+		old.removeChild(old.firstChild)
+	}
+	old.appendChild(newtype)
+}
+
+//新增设备
+var addDevice = function(obj)
+{
+	id = obj.name
+	oldtype = id + '_newtype'
+	oldmac = id + '_newmac'
 	
-	//删除用户
-	var delUser = function(obj){
-		var r = confirm("您确定要删除该设备？")
-		if (r == true)
-		{
-			method = 'delUser'
-			id = obj.name
-			string = "id=" + id + "&method=" +method
-			ajax('/openms/accept.php',check,string)
-		}		
-	}
+	changeId(oldtype, initSelect(typeArr))
+	
+	var input =document.createElement('input')
+	input.setAttribute('type' , 'text')
+	
+	changeId(oldmac, input)
+	obj.setAttribute('value','确定')
+	obj.setAttribute('onclick','Submit()')
+}
 
-	//更改用户总流量
-	var changeFlow = function(obj){
-		a = obj.parentNode.parentNode
-		a.childNodes[1].removeChild(a.childNodes[1].childNodes[0])
+//更改流量
+var changeFlow = function(obj)
+{
+	id = obj.name
+	oldflow = id + '_flow'
 
-		var arr = ['1','2','3','4','5']
-		var select = initSelect(arr)
+	changeId(oldflow, initSelect(flowArr))
 
-		a.childNodes[1].appendChild(select)
+	obj.setAttribute('value','确定')
+	obj.setAttribute('onclick','Submit()')
+}
 
-		a.childNodes[8].childNodes[0].setAttribute('value','确定')
-		a.childNodes[8].childNodes[0].setAttribute('onclick','changeFlowSub(this)')
-	}
 
-	//更改用户流量后通过ajax方法提交
-	var changeFlowSub = function(obj) {
-		a = obj.parentNode.parentNode
-		select =  a.childNodes[1].childNodes[0]
-		flow = select.options[select.selectedIndex].text
+//修改Mac
+var changeMac = function(obj)
+{
+	id = obj.name
+	oldmac = id + '_mac'
+
+	var input =document.createElement('input')
+	input.setAttribute('type' , 'text')
+	input.setAttribute('value', document.getElementById(oldmac).firsttChild.nodeValue)
+	changeId(oldmac, input)
+	obj.setAttribute('value','确定')
+	obj.setAttribute('onclick','Submit()')
+}
+	
+//删除用户
+var delUser = function(obj){
+	var r = confirm("您确定要删除该设备？")
+	if (r == true)
+	{
+		method = 'delUser'
 		id = obj.name
-		method = 'changeFlow'
-		string = "flow=" + flow + "&id=" + id  + "&method=" + method
-		ajax('/openms/accept.php', check, string)
-		
-	}
+		string = "id=" + id + "&method=" +method
+		ajax('/openms/accept.php',check,string)
+	}		
+}
 
-	//删除设备
-	var delDev = function(obj){
-		var r = confirm("您确定要删除该设备？")
-		if (r == true)
-		{
-			a = obj.parentNode.parentNode
-			mac = a.childNodes[1].childNodes[0].nodeValue	
-			method = 'delDev'
-			id = obj.name
-			string = "mac=" + mac + "&id=" + id + "&method=" +method
-			ajax('/openms/accept.php',check,string)
-		}		
-	}
+//删除设备
+var delDevice = function(obj){
+	var r = confirm("您确定要删除该设备？")
+	if (r == true)
+	{
+		id = obj.name
+		id_mac = id +  '_mac'
+		mac = document.getElementById(id_mac).firstChild
+		method = 'delDev'
+		string = "mac=" + mac + "&id=" + id + "&method=" +method
+		ajax('/openms/accept.php',check,string)
+	}		
+}
 
-	//增加用户
-	var addUser = function(obj){
-		a = obj.parentNode.parentNode
-
-		var input = document.createElement("input")
-		input.setAttribute('type', 'text')	
-		a.childNodes[0].appendChild(input)
-
-		arr = ['1','2','3','4','5']
-		select = initSelect(arr)
-		a.childNodes[1].appendChild(select)
-
-		a.childNodes[3].childNodes[0].setAttribute('value','确定')
-		a.childNodes[3].childNodes[0].setAttribute('onclick','addUserSubmit(this)')
-	}
+var Submit = function()
+{}
+//更改用户流量后通过ajax方法提交
+var changeFlowSub = function(obj) {
+	a = obj.parentNode.parentNode
+	select =  a.childNodes[1].childNodes[0]
+	flow = select.options[select.selectedIndex].text
+	id = obj.name
+	method = 'changeFlow'
+	string = "flow=" + flow + "&id=" + id  + "&method=" + method
+	ajax('/openms/accept.php', check, string)
 	
-	//更改设备的mac地址
-	var change = function(obj){
-		a = obj.parentNode.parentNode
-		oldmac = a.childNodes[1].childNodes[0].nodeValue	
-		a.childNodes[1].removeChild(a.childNodes[1].childNodes[0])
-		var input = document.createElement("input")
-		input.setAttribute('type', 'text')	
-		input.setAttribute('value', oldmac)	
-		a.childNodes[1].appendChild(input)
+}
 
-		a.childNodes[4].childNodes[0].setAttribute('value','确定')
-		a.childNodes[4].childNodes[0].setAttribute('onclick','changeSubmit(this)')
-	}
+
 	
 	//增加设备后通过ajax方法提交
 	var addSubmit = function(obj){
--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<link rel="stylesheet" type="text/css" href="mystyle.css" />
 	</head>
-<?php
-	$json_string = file_get_contents("data.json");
-	$json = json_decode($json_string, true);
-?>
 	<table>
 		<tr>
 			<td>姓名</td>
@@ -19,52 +15,84 @@
 			<td colspan='4'>操作</td>
 		</tr>
 <?php 
+	$json_string = file_get_contents("data.json");
+	$json = json_decode($json_string, true);
+
+	$total_flow = 0;
+	$total_out = 0;
+	$total_in = 0;
+
 	foreach ($json['club'] as $k => $v)
 	{
-			$device_n	= count($v['device']) + 1;
-			$id			= $v['id'];
-			$name		= $v['name'];
-			$total		= $v['total'];
-			$nowflow	= $v['nowflow'];
+		$device_n	= count($v['device']) + 1;
+		$id		= $v['id'];
+		$name		= $v['name'];
+		$total		= (int)($v['total'] / 1024 / 1024);
+		$nowflow	= (int)($v['nowflow'] / 1024 / 1024);
+		$total_flow    += $v['nowflow'];
+
+		$id_flow = $id . '_flow';
+		$id_type = $id . '_newtype';
+		$id_mac = $id . '_newmac';
+
+		echo "<tr>";
+		echo "<td rowspan='$device_n'>$name</td>";
+		echo "<td id='$id_flow' rowspan='$device_n'>$total</td>";
+		echo "<td rowspan='$device_n'>$nowflow</td>";
+		echo "<td id='$id_type'></td>";
+		echo "<td id='$id_mac'></td>";
+		echo "<td></td>";
+		echo "<td></td>";
+		echo "<td colspan='2'><input type='button' value='新增设备' name='$id'  onclick='addDevice(this)'/></td>";
+		echo "<td rowspan='$device_n'><input type='button' value='修改流量' name='$id'  onclick='changeFlow(this)'/></td>";
+		echo "<td rowspan='$device_n'><input type='button' value='删除用户' name='$id'  onclick='delUser(this)'/></td>";
+		echo "</tr>";
+		foreach($json['club'][$k]['device'] as $key=>$value)
+		{
+			$type = $value['type'];
+			$mac = $value['mac'];
+			$out = (int)($value['out'] / 1024 / 1024);
+			$in = (int)($value['in'] / 1024 / 1024);
+
+
+			$id_type = $id.'_'.$key . '_type';
+			$id_mac = $id .'_'.$key . '_mac';
+
+			$id_key = $id . '_' . $key;
+
+			$total_out += (int)$value['out'];
+			$total_in += (int)$value['in'];
 			echo "<tr>";
-			echo "<td rowspan='$device_n'>$name</td>";
-			echo "<td rowspan='$device_n'>$total</td>";
-			echo "<td rowspan='$device_n'>$nowflow</td>";
-			echo "<td></td>";
-			echo "<td></td>";
-			echo "<td></td>";
-			echo "<td></td>";
-			echo "<td colspan='2'><input type='button' value='新增设备' name='$id'  onclick='add(this)'/></td>";
-			echo "<td rowspan='$device_n'><input type='button' value='修改流量' name='$id'  onclick='changeFlow(this)'/></td>";
-			echo "<td rowspan='$device_n'><input type='button' value='删除用户' name='$id'  onclick='delUser(this)'/></td>";
+			echo "<td id='$id_type'>$type</td>";
+			echo "<td id='$id_mac'>$mac</td>";
+			echo "<td>$out</td>";
+			echo "<td>$in</td>";
+			echo "<td><input type='button' value='修改Mac' name='$id_key' onclick='changeMac(this)'/></td>";
+			echo "<td><input type='button' value='删除设备' name='$id_key' onclick='delDevice(this)'/></td>";
 			echo "</tr>";
+
+		}
 	}
-	/*
-		</tr>
-		<tr>
-			<td>"+i.type+"</td>
-			<td>"+i.mac+"</td>
-			<td>"+up+"</td>
-			<td>"+down+"</td>
-			<td><input type='button' value='修改Mac' name='"+obj.club[x].id+"' onclick='change(this)'/></td>
-			<td><input type='button' value='删除设备' name='"+obj.club[x].id+"' onclick='delDev(this)'/></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td></td>
-			<td colspan='5'></td>
-			<td colspan='4'><input type='button' value='新增用户' name='" + nowID + "' onclick='addUser(this)' /></td>
-		</tr>
-		<tr>
-			<td >总计</td>
-			<td>30720</td>
-			<td>"+total['flow']+"</td>
-			<td colspan='2'></td>
-			<td>"+total['up']+"</td>
-			<td>"+total['down']+"</td>
-			<td colspan='4'><input type='button' value='清零流量' onclick='cleanFlow()' /></td>
-			</tr>
-	 */
+
+	$id = $id + 1;
+	$total_flow = (int)($total_flow / 1024 / 1024);
+	$total_out = (int)($total_out / 1024 / 1024);
+	$total_in = (int)($total_in / 1024 / 1024);
+	echo "<tr>";
+	echo "<td></td>";
+	echo "<td></td>";
+	echo "<td colspan='5'></td>";
+	echo "<td colspan='4'><input type='button' value='新增用户' name='$id' onclick='addUser(this)' /></td>";
+	echo "</tr>";
+	echo "<tr>";
+	echo "<td >总计</td>";
+	echo "<td>30720</td>";
+	echo "<td>$total_flow</td>";
+	echo "<td colspan='2'></td>";
+	echo "<td>$total_out</td>";
+	echo "<td>$total_in</td>";
+	echo "<td colspan='4'><input type='button' value='清零流量' onclick='cleanFlow()' /></td>";
+	echo "</tr>";
 ?>
 	</table>
 	<script src="index.js"></script>
